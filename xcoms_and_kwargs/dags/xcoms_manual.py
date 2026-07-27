5151596f5667f9445fac5c4756a875da71610a7d
Shah Rahman <test.pinecone.user@proton.me>
2026-07-27 07:55:05 +0300

    fix: add task_id to fetch_data and process_data tasks in xcoms_manual DAG

--- xcoms_and_kwargs/dags/xcoms_manual.py
+++ xcoms_and_kwargs/dags/xcoms_manual.py
@@ -3,7 +3,7 @@ from airflow.decorators import dag, task
 @dag
 def xcoms_manual():
 
-    @task.python
+    @task.python(task_id="task1")
     def fetch_data(ti):
         # Simulate fetching data from an external source/api
         data = {"name": "Airflow", "version": "3.0"}
@@ -12,4 +12,10 @@ def xcoms_manual():
         # Push the data to XCom
         ti.xcom_push(key="fetched_data", value=data)
 
-    
+    @task.python(task_id="task2")
+    def process_data(ti):
+        # Pull the data from XCom
+        pulled_data = ti.xcom_pull(key="fetched_data", task_ids="task1")
+        # simulate processing the data
+        processed_data = f"Processed {pulled_data['name']} version {pulled_data['version']}"
+        print(processed_data)
